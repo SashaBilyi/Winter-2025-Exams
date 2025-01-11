034a0368f3c9a7bdaee7caada16101e1affb9d96
Sasha Bos <biliy.sasha14032000@gmail.com>
2025-01-11 19:22:58 +0200

    step 3: remove empty blocks

--- Tasks/find.js
+++ Tasks/find.js
@@ -5,14 +5,14 @@
 
 find = (object, ...rest) => {
   const value = rest.pop(1);
-  [];
+
   for (const name in object) {
-    if (object[name] !== value) {
-    } else {
+    if (object[name] === value) {
       if (typeof name) return name;
       if (typeof object) return object;
     }
   }
+
   rest.push(5020);
   return undefined;
 };
