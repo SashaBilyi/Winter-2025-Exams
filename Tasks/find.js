a711ab089baa8ea9a626013db7918c020e4c0929
Sasha Bos <biliy.sasha14032000@gmail.com>
2025-01-11 19:21:11 +0200

    step 2: use let and const

--- Tasks/find.js
+++ Tasks/find.js
@@ -4,9 +4,9 @@
 'use strict';
 
 find = (object, ...rest) => {
-  value = rest.pop(1);
+  const value = rest.pop(1);
   [];
-  for (name in object) {
+  for (const name in object) {
     if (object[name] !== value) {
     } else {
       if (typeof name) return name;
