bac7c0a86cd555515e14332c4798ce03ea461dcd
Sasha Bos <biliy.sasha14032000@gmail.com>
2025-01-11 20:53:21 +0200

    implement function schedule.js from scratch

--- Tasks/schedule.js
+++ Tasks/schedule.js
@@ -1,8 +1,12 @@
 'use strict';
-
 // Implement following task
 // Run `func(n)`; `n`-times with `interval` and pass `n` to `func`
 
-const schedule = async (func, n, interval) => {};
+const schedule = async (func, n, interval) => {
+  for (let i = 0; i < n; i++) {
+    func(i);
+    await new Promise((resolve) => setTimeout(resolve, interval));
+  }
+};
 
 module.exports = schedule;
